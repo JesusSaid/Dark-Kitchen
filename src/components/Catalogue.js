9cf6464fdb6739684968a5195f51e75906139eae
Leonel Santiago Rosas <lsantiago@ittiva.com>
2024-05-27 10:17:35 -0600

    Agregando la nueva tarjeta

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import SanityClient from "../client.js"
 import Style from "../styles/catologo.module.css"
-export default function Catalogue(){
+export default function Catalogue() {
     const [catalogueData, setCatalogueData] = useState(null);
 
     useEffect(() => {
@@ -31,47 +31,47 @@ export default function Catalogue(){
                 </h1>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {catalogueData && catalogueData.map((catalogue, index) => (
-                    <article>
-                    
-                        <Link to = {"/catalogue/" + catalogue.titulo.current} key = {catalogue.titulo.current}>
-                            <span
-                                className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
-                                key = {index}
-                            >
-                                <img
-                                    src = {catalogue.imagen.asset.url}
-                                    alt = {catalogue.imagen.alt || catalogue.titulo}
-                                    className="w-full h-full rounded-r object-cover absolute"
-                                />
-                                <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                    <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
-                                        {catalogue.titulo}
-                                    </h3>
+                        <article>
+
+                            <Link to={"/catalogue/" + catalogue.titulo.current} key={catalogue.titulo.current}>
+                                <span
+                                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                                    key={index}
+                                >
+                                    <img
+                                        src={catalogue.imagen.asset.url}
+                                        alt={catalogue.imagen.alt || catalogue.titulo}
+                                        className="w-full h-full rounded-r object-cover absolute"
+                                    />
+                                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                                        <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                                            {catalogue.titulo}
+                                        </h3>
+                                    </span>
                                 </span>
-                            </span>
-                        </Link>
-                    </article>
+                            </Link>
+                        </article>
                     ))}
                 </div>
             </section>
             <div className={Style.card}>
-                                <div className={Style.cardimg}></div>
-                                <div className={Style.cardinfo}>
-                                    <p className={Style.texttitle}>Pastel</p>
-                                    <p class="text-body">Descripcion del paster</p>
-                                </div>
-                                <div className={Style.cardfooter}>
-                                <span className={Style.texttitle}>$499.49</span>
-                                <div className={Style.cardbutton}>
-                                    <svg className={Style.svgicon} viewBox="0 0 20 20">
-                                    <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
-                                    <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
-                                    <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
+                <div className={Style.cardimg}></div>
+                <div className={Style.cardinfo}>
+                    <p className={Style.texttitle}>Pastel</p>
+                    <p class="text-body">Descripcion del paster</p>
+                </div>
+                <div className={Style.cardfooter}>
+                    <span className={Style.texttitle}>$499.49</span>
+                    <div className={Style.cardbutton}>
+                        <svg className={Style.svgicon} viewBox="0 0 20 20">
+                            <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+                            <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+                            <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </main>
-        
+
     );
 }

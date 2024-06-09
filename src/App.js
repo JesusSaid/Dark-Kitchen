940d0bfd2e8fad6e2e1c58f0214963d90931595e
Omar Cruz <omar.cruzr97@hotmail.com>
2024-06-08 18:52:21 -0600

    adding footer

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import ProductDetails from "./components/ProductDetails";
 import Eventos from "./components/eventos";
 import VerPedidos from "./components/VerPedidos";
 import ProtectedRoute from './components/ProtectedRoute';
+import Footer from "./components/footer";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<Catalogue />} />
-        <Route path="/personalizar_pastel" element={<PersonalizarPastel />} />
-        <Route path="/yo" element={<Perfil />} />
-        <Route path="/producto/:productId" element={<ProductDetails />} />
-        <Route path="/eventos" element={<Eventos />} />
-        <Route path="/verPedidos" element={<ProtectedRoute element={VerPedidos} allowedTypes={[1]} />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogue />} />
+          <Route path="/personalizar_pastel" element={<PersonalizarPastel />} />
+          <Route path="/yo" element={<Perfil />} />
+          <Route path="/producto/:productId" element={<ProductDetails />} />
+          <Route path="/eventos" element={<Eventos />} />
+          <Route path="/verPedidos" element={<ProtectedRoute element={VerPedidos} allowedTypes={[1]} />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 

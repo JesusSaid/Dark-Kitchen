11737a61a6930f51efff662102485d817c725c26
Damanger <omar.cruzr97@hotmail.com>
2024-06-12 11:31:37 -0600

    updating error404

--- src/components/error.js
+++ src/components/error.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const error = () => {
     return (
-        <div style={{display:'grid', placeItems:'center', textAlign:'center'}}>
+        <div style={{display:'grid', placeItems:'center', textAlign:'center', width:'100vw', height:'100vh'}}>
             <h1 style={{fontSize:'5rem'}}>404 Not Found</h1>
             <h3 style={{fontSize:'2.5rem'}}>Lo sentimos, la página que buscas no existe.</h3>
             <img src='./candle.gif'></img>
